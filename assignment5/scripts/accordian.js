66a5df003a1369fe8b07008f291fe35cd04154a4
Preeti Chavan <preeti.chavan@aurea.com>
2020-01-09 19:21:25 +0530

    Initial Commit

---
+++ assignment5/scripts/accordian.js
@@ -0,0 +1,18 @@
+(function() {
+var acc = $(".accordion");
+acc.each(function(){
+$(this).on("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+});
+})(jQuery);
